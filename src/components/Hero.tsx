56c45287b7f8fbf694f8443e111e8ec9bf31fc56
KiNG <odon66674@gmail.com>
2025-01-01 19:31:16 +0100

    Change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
-    <section id='home' className="bg-black py-[72px] sm:py-24 overflow-clip text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] relative " id="">
+    <section id='home' className="bg-black py-[72px] sm:py-24 overflow-clip text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] relative " >
       <div className="absolute h-[375px] w-[750px] sm:h-[750px] sm:w-[1536px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] "></div>
       <div className="container relative">
         <div className="flex justify-center items-center">
